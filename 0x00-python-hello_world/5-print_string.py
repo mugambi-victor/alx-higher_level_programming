41c252b4755e171ade8a428a9cbac504d378b307
mugambi-victor <victormmugambi@gmail.com>
2023-10-30 14:17:07 +0300

    update

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 str_value = "Holberton School"
-
-# Print 3 times the value of str followed by a new line
 print(str_value * 3)
-
-# Print the 9 first characters of str followed by a new line
 print(str_value[:9])
