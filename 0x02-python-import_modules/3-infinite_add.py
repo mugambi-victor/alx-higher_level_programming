1d7a45604dd4c568fe5611a0e701dabf1f02e638
mugambi-victor <victormmugambi@gmail.com>
2023-11-02 14:26:53 +0300

    tasks

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    if not args:
+        print("0")
+    else:
+        total = 0
+        for i in range(len(sys.argv) - 1):
+            total += int(sys.argv[i + 1])
+            print("{}".format(total))
