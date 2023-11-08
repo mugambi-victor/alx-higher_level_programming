a3cc1e7bdf9bd14c9933e3eb6c77f95f7356eef6
mugambi-victor <victormmugambi@gmail.com>
2023-11-08 16:22:29 +0300

    tasks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,5 +5,4 @@ def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             new_matrix[i][j] = matrix[i][j] ** 2
-
     return new_matrix
