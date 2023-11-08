de32b451615903b785c6c41e26b90b60761fe732
mugambi-victor <victormmugambi@gmail.com>
2023-11-08 16:18:02 +0300

    tasks

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_numbers = set()
+
+    for num in my_list:
+        unique_numbers.add(num)
+
+    result = sum(unique_numbers)
+
+    return result
