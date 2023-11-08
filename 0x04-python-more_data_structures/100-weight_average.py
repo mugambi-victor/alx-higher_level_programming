0c9715d1a025adadf08ce193fd5a2ca372992fc0
mugambi-victor <victormmugambi@gmail.com>
2023-11-08 16:44:51 +0300

    pycodestyle

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,5 @@ def weight_average(my_list=[]):
     
     weighted_sum = sum(score * weight for score, weight in my_list)
     total_weight = sum(weight for _, weight in my_list)
-    
     weighted_average = weighted_sum / total_weight
-
     return weighted_average
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
+
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
 
@@ -14,5 +14,4 @@ def roman_to_int(roman_string):
             result -= current_value
         else:
             result += current_value
-
     return result
