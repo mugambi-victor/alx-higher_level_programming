eb769bdb4293c6f1f1c0f915b7db9fb3d35f3c68
mugambi-victor <victormmugambi@gmail.com>
2023-11-21 15:24:04 +0300

    task two

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+# 0-square.py
+"""Define a class Square."""
+
+
 class Square:
-    """
-    This is an empty class definition for Square.
-    """
+    """Represent a square."""
     pass
