c718cb8d5aefe0648f22f94b26a53694bc1b5e94
mugambi-victor <victormmugambi@gmail.com>
2023-11-21 15:33:56 +0300

    task four

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# 3-square.py
+"""Define a class Square."""
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): Private attribute representing the size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
