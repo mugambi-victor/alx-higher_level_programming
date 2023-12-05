03232fe8210fcd5ce75616052b485c05e450cd57
mugambi-victor <victormmugambi@gmail.com>
2023-12-05 15:47:49 +0300

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,20 +4,18 @@ import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    filename = "add_item.json"
 
-filename = "add_item.json"
+    try:
+        # Load existing items from the file, if it exists
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        # If the file doesn't exist, create an empty list
+        my_list = []
 
-try:
-    # Load existing items from the file, if it exists
-    my_list = load_from_json_file(filename)
-except FileNotFoundError:
-    # If the file doesn't exist, create an empty list
-    my_list = []
+    # Add command line arguments to the list
+    my_list.extend(sys.argv[1:])
 
-# Add command line arguments to the list
-my_list.extend(sys.argv[1:])
-
-# Save the updated list to the file
-save_to_json_file(my_list, filename)
+    # Save the updated list to the file
+    save_to_json_file(my_list, filename)
