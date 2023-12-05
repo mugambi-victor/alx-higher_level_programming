fab83a20026d68f01314587f9722cbafe038e8fb
mugambi-victor <victormmugambi@gmail.com>
2023-12-05 15:54:19 +0300

    tasks

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Student module
+"""
+
+
+class Student:
+    """Defines a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary containing
+            the attributes of the Student instance.
+        """
+        return self.__dict__
